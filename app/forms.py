eee3cd883637189ed124e23cf2a7143f8ed5a545
ol.podol <ol.podolskyi@gmail.com>
2015-06-15 00:17:54 +0300

    Login OK!

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,9 @@
 from flask.ext.wtf import Form
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Required, Email, Length
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import DataRequired, Required, Email
 
 
+class LoginForm(Form):
+    username = StringField('username', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
+    remember_me = BooleanField('remember_me', default=False)
--- app/models.py
+++ app/models.py
@@ -15,6 +15,21 @@ class User(db.Model):
         self.email = email
         self.password = password
 
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        try:
+            return unicode(self.id)  # python 2
+        except NameError:
+            return str(self.id)  # python 3
+
     def __repr__(self):
         return '<User %r>' % (self.username)
 
